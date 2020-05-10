8ca928812c0a706736f561b85763391fee614ae0
Sviatlana <sviatlana.balabanovich@gmail.com>
2020-05-10 21:36:57 +0300

    Added HW5 + diamond syntax

--- src/hw5/Task3/ShowElement.java
+++ src/hw5/Task3/ShowElement.java
@@ -2,15 +2,15 @@ package hw5.Task3;
 
 public class ShowElement {
     public static void main(String[] args) {
-        Array<Boolean> booleanArray = new Array<Boolean>(ArrayExamples.booleans);
-        Array<Byte> bytesArray = new Array<Byte>(ArrayExamples.bytes);
-        Array<Short> shortsArray = new Array(ArrayExamples.shorts);
-        Array<Integer> intsArray = new Array(ArrayExamples.ints);
-        Array<Long> longsArray = new Array(ArrayExamples.longs);
-        Array<Float> floatsArray = new Array(ArrayExamples.floats);
-        Array<Double> doublesArray = new Array(ArrayExamples.doubles);
-        Array<Character> charsArray = new Array(ArrayExamples.chars);
-        Array<String> stringsArray = new Array(ArrayExamples.strings);
+        Array<Boolean> booleanArray = new Array<>(ArrayExamples.booleans);
+        Array<Byte> bytesArray = new Array<>(ArrayExamples.bytes);
+        Array<Short> shortsArray = new Array<>(ArrayExamples.shorts);
+        Array<Integer> intsArray = new Array<>(ArrayExamples.ints);
+        Array<Long> longsArray = new Array<>(ArrayExamples.longs);
+        Array<Float> floatsArray = new Array<>(ArrayExamples.floats);
+        Array<Double> doublesArray = new Array<>(ArrayExamples.doubles);
+        Array<Character> charsArray = new Array<>(ArrayExamples.chars);
+        Array<String> stringsArray = new Array<>(ArrayExamples.strings);
 
         System.out.println(booleanArray.returnElement(1));
         System.out.println(bytesArray.returnElement(3));
